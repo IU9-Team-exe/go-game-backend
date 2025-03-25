54eb623a621f9a5a6762efe5800f82fe09e093cf
FunnyRockfish <List.kedra.79@gmail.com>
2025-03-25 23:29:36 +0300

    add 1x1 lite

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"team_exe/internal/adapters"
 	"team_exe/internal/bootstrap"
 	authDelivery "team_exe/internal/delivery/auth"
+	gameDelivery "team_exe/internal/delivery/game"
 	katagoDelivery "team_exe/internal/delivery/katago"
 	katagoProto "team_exe/microservices/proto"
 )
@@ -18,10 +23,12 @@ import (
 type mainDeliveryHandler struct {
 	auth   *authDelivery.AuthHandler
 	katago *katagoDelivery.KatagoHandler
+	game   *gameDelivery.GameHandler
 }
 
-type databases struct {
-	redisClient *redis.Client
+type dataBaseAdapters struct {
+	redisAdapter *adapters.AdapterRedis
+	mongoAdapter *adapters.AdapterMongo
 }
 
 func main() {
@@ -32,19 +39,24 @@ func main() {
 		return
 	}
 
-	r := chi.NewRouter()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	grpcKatago, err := grpc.Dial(
-		"host.docker.internal:8082",
-		grpc.WithInsecure(),
-	)
+	go handleShutdown(cancel, logger)
+
+	databaseAdapters := initDatabaseAdapters(ctx, logger, *cfg)
+	defer databaseAdapters.mongoAdapter.Close(ctx)
+	defer databaseAdapters.redisAdapter.Close(ctx)
+
+	grpcKatago, err := grpc.Dial("host.docker.internal:8082", grpc.WithInsecure())
 	if err != nil {
 		logger.Fatal("Failed to dial grpc", zap.Error(err))
 	}
 	defer grpcKatago.Close()
 
-	mainDeliveryHandlers := initializeDeliveryHandlers(*cfg, logger, grpcKatago)
-	mainDeliveryHandlers.Router(r)
+	r := chi.NewRouter()
+	handlers := initializeDeliveryHandlers(ctx, *cfg, logger, grpcKatago, databaseAdapters)
+	handlers.Router(r)
 
 	port := ":8080"
 	logger.Infof("Server is running on port %s", port)
@@ -58,7 +70,6 @@ func NewLogger() *zap.SugaredLogger {
 	if err != nil {
 		panic("failed to initialize logger: " + err.Error())
 	}
-
 	return logger.Sugar()
 }
 
@@ -68,42 +79,55 @@ func (h *mainDeliveryHandler) Router(r *chi.Mux) {
 	r.Post("/login", h.auth.Login)
 	r.Delete("/logout", h.auth.Logout)
 	r.Post("/autoBotGenerateMove", h.katago.HandleGenerateMove)
+	r.Post("/NewGame", h.game.HandleNewGame)
+	r.Post("/JoinGame", h.game.HandleJoinGame)
+	r.Get("/startGame", h.game.HandleStartGame)
 }
 
-func initRedis(log *zap.SugaredLogger, cfg bootstrap.Config) *redis.Client {
-	redisURL := os.Getenv("REDIS_URL")
-	log.Info("Connecting to Redis at:", redisURL)
-
-	client := redis.NewClient(&redis.Options{
-		Addr:     redisURL,
-		Password: "",
-		DB:       0,
-	})
-
-	if _, err := client.Ping(context.Background()).Result(); err != nil {
-		log.Fatal("failed to connect Redis", zap.Error(err))
+func initDatabaseAdapters(ctx context.Context, log *zap.SugaredLogger, cfg bootstrap.Config) *dataBaseAdapters {
+	mongoAdapter := adapters.NewAdapterMongo(&cfg)
+	if err := mongoAdapter.Init(ctx); err != nil {
+		log.Fatal("Не удалось инициализировать MongoDB", zap.Error(err))
 	}
 
-	log.Info("Redis connection established")
-	return client
-}
+	redisAdapter := adapters.NewAdapterRedis(&cfg)
+	if err := redisAdapter.Init(ctx); err != nil {
+		log.Fatal("Не удалось инициализировать Redis", zap.Error(err))
+	}
 
-func initDatabase(log *zap.SugaredLogger, cfg bootstrap.Config) *databases {
-	return &databases{
-		redisClient: initRedis(log, cfg),
+	log.Info("Адаптеры баз данных инициализированы")
+	return &dataBaseAdapters{
+		redisAdapter: redisAdapter,
+		mongoAdapter: mongoAdapter,
 	}
 }
 
-func initializeDeliveryHandlers(cfg bootstrap.Config, log *zap.SugaredLogger, grpcKatago *grpc.ClientConn) *mainDeliveryHandler {
-	databaseStorage := initDatabase(log, cfg)
-
+func initializeDeliveryHandlers(
+	ctx context.Context,
+	cfg bootstrap.Config,
+	log *zap.SugaredLogger,
+	grpcKatago *grpc.ClientConn,
+	databaseAdapters *dataBaseAdapters,
+) *mainDeliveryHandler {
 	katagoManager := katagoProto.NewKatagoServiceClient(grpcKatago)
 	katagoDeliveryHandler := katagoDelivery.NewKatagoHandler(cfg, log, katagoManager)
 
-	authDeliveryHandler := authDelivery.NewMapAuthHandler(databaseStorage.redisClient)
+	gameDeliveryHandler := gameDelivery.NewGameHandler(cfg, log, databaseAdapters.mongoAdapter, databaseAdapters.redisAdapter)
+
+	authDeliveryHandler := authDelivery.NewMapAuthHandler(databaseAdapters.redisAdapter)
 
 	return &mainDeliveryHandler{
 		auth:   authDeliveryHandler,
 		katago: katagoDeliveryHandler,
+		game:   gameDeliveryHandler,
 	}
+}
+
+func handleShutdown(cancelFunc context.CancelFunc, log *zap.SugaredLogger) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	log.Info("Received shutdown signal")
+	cancelFunc()
+	time.Sleep(1 * time.Second) // дать время закрыть соединения
 }
