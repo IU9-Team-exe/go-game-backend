d3f6b635108d814cb3cdd75f649fc75fb4b99b07
Artem Zhuk <artem>
2025-03-26 17:04:25 +0300

    fixed cors gggoll

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		header := w.Header()
-		header.Add("Access-Control-Allow-Origin", "http://localhost:3000")
+		header.Add("Access-Control-Allow-Origin", "*")
 		header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
 		header.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 		header.Add("Access-Control-Allow-Credentials", "true")
